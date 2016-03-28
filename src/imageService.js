f613b69b16f53fb01e3ee61a65a5544172421fc2
S K <xz64@users.noreply.github.com>
2016-03-27 20:13:46 -0400

    add recent search caching

--- src/imageService.js
+++ src/imageService.js
@@ -4,6 +4,8 @@ var IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
 var IMGUR_ENDPOINT = 'https://api.imgur.com/3/gallery/t/';
 
 var imageService = {
+  RECENT_SEARCH_LIMIT: 20,
+  recentSearches: [],
   query: function query(queryString, pageNumber) {
     pageNumber = !isNaN(pageNumber) ? pageNumber : 0;
     var request_options = {
@@ -16,6 +18,7 @@ var imageService = {
 
     return rp(request_options)
     .then(function(data) {
+      this.addRecentSearch(queryString);
       return this.transformJSON(data);
     }.bind(this))
     .catch(function(err) {
@@ -30,6 +33,15 @@ var imageService = {
         pageUrl: 'http://i.imgur.com/' + item.id
       };
     });
+  },
+  addRecentSearch: function addRecentSearch(queryString) {
+    this.recentSearches.push({
+      query: queryString,
+      time: new Date().toISOString()
+    });
+    if(this.recentSearches.length > this.RECENT_SEARCH_LIMIT) {
+      this.recentSearches.shift();
+    }
   }
 };
 
