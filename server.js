1b9ee61131057b63bb8109a3d533c2ab6f50b27a
S K <xz64@users.noreply.github.com>
2016-03-27 20:20:15 -0400

    add image search to server

--- server.js
+++ server.js
@@ -5,4 +5,13 @@ var imageService = require('./src/imageService.js');
 var app = express();
 var port = +process.env.PORT || 8080;
 
+app.get('/api/imagesearch/:query', function(req, res) {
+  var query = req.params.query;
+  var page = req.query.offset;
+  imageService.query(query, page)
+  .then(function(data) {
+   res.json(data);
+  });
+});
+
 app.listen(port);
